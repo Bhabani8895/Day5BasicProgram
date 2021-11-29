9b1822f2e4ea50b11d39c04c6e7b7103e12fd73d
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2021-11-30 00:40:54 +0530

    Program added in Branch8

---
+++ BasicProgram/QuotientAndRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class QuotientAndRemainder
+    {
+        public void QAndR()
+        {
+            Console.WriteLine("Enter Dividend");
+            int Dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+            int Quotient = Convert.ToInt32(Dividend / Divisor);
+            Console.WriteLine("Quotient=" + Quotient);
+            int Remainder = Convert.ToInt32(Dividend % Divisor);
+            Console.WriteLine("Remainder=" + Remainder);
+        }
+    }
+}
