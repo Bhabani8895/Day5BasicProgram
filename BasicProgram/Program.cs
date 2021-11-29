d93c307bfcc717de1489d79acbf55fda39433950
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2021-11-29 09:51:24 +0530

    Program commited to Branch3

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -1,12 +1,13 @@
 ﻿using BasicProgram;
 
-Console.WriteLine("\tEnter any key to run and n to End the program!!!");
+Console.WriteLine("\tEnter any key to run and n to End the program");
 char choice = Convert.ToChar(Console.ReadLine());
 while (choice != 'n')
 {
     Console.WriteLine("\t(((((Choose any one!)))))");
     Console.WriteLine("\t(((((1.Flip Coin )))))))))");
     Console.WriteLine("\t(((((2.Leap Year)))))");
+    Console.WriteLine("\t(((((3.Prime Factors)))))");
     int n = Convert.ToInt32(Console.ReadLine());
     switch (n)
     {
@@ -17,6 +18,10 @@ while (choice != 'n')
         case 2:
             LeapYear leapYear = new LeapYear();
             leapYear.Year();
+            break;
+        case 3:
+            PrimeFactor obj = new PrimeFactor();
+            obj.Prime();
             break;
     }
 }
